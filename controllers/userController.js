c2f632e1b4ac8ab2ec0b216cfe2e51078ca6feb3
devAbreu <yolfrank.abreu@gmail.com>
2023-06-20 13:20:49 -0500

    :sparkles: added userController router to test the verifytoken middleware

---
+++ controllers/userController.js
@@ -0,0 +1,12 @@
+const { verifyToken } = require("../middleware/authJwt");
+const { User } = require("../models/user.model");
+
+module.exports.userInfo = async (req, res, next) => {
+   let user = req.user;
+   const userObj = await User.findOne({ username: user.username }).select('-password -__v');;
+   if (userObj) {
+      res.status(200).json({ "response": userObj });
+   } else {
+      res.status(404).json({ "response": "Not found. (user does not exist with this username)!" })
+   }
+}
