58727fc1a7c33a708f3913719ac0dc7069e90fa6
devAbreu <yolfrank.abreu@gmail.com>
2023-06-20 13:18:46 -0500

    :sparkles: added dotenv to load sensitive data from .env file

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,29 +1,39 @@
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 
 const { User } = require("../models/user.model");
 
 module.exports.loginUser = async (req, res, next) => {
     //todo: add jwt
-    if (req.method === "POST") {
-        const { username, password } = req.body;
-        const user = await User.findOne({ username: username });
-        if (user && await bcrypt.compare(password, user.password)) {
-            res.status(200).json({ "response": "logged in." });
-        } else {
-            res.status(401).json({ "response": "Unauthorized. (user does not exist with this username or worg password)!" })
+        if (req.method === "POST") {
+            const { username, password } = req.body;
+            console.log(req.body.password)
+            const user = await User.findOne({ username: username });
+            console.log(user.password)
+            if (user && await bcrypt.compare(password, user.password)) {
+                let payload = {
+                    id: user.id,
+                    username: user.username,
+                };
+                let token = jwt.sign(payload, '36b7abb67d420eaaf3e526b0fe4d1b9b', { expiresIn: '24h' });
+                res.status(200).json({ "response": "logged in.", "token": token });
+            } else {
+                res.status(401).json({ "response": "Unauthorized. (user does not exist with this username or worg password)!" })
+            }
         }
-    }
+
 }
 
 module.exports.registerUser = async (req, res, next) => {
     if (req.method === "POST") {
         const { username, email, password } = req.body;
+        console.log(req.body);
         const foundUser = await User.findOne({ "username": username });
         if (foundUser) {
             res.status(409).json({ "response": "duplication error (user already exist with this username)." });
         } else {
             if (password.length >= 8) {
-                bcrypt.hash("password", 10, (err, hash) => {
+                bcrypt.hash(password, 10, (err, hash) => {
                     if (!err) {
                         const newUser = new User({
                             username: username,
