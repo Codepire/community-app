4b16f4dc17f3007efcb47ffd5e827b55c49d7bd2
devAbreu <yolfrank.abreu@gmail.com>
2023-06-20 23:44:01 -0500

    :sparkles: added verifytoken middleware

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const { userInfo } = require('../controllers/userController');
-const { verifyToken } = require('../middleware/authJwt');
+const { verifyToken } = require('../middlewares/authJwt');
 
 router.route("/me").get(verifyToken, userInfo);
 
