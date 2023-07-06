515d0a5c882b7f09bc3a7178933076ce695a60aa
Viral Rupani <viralrupani12017@gmail.com>
2023-07-06 18:49:49 +0530

    [feat] add create server endpoint

--- controllers/serverController.js
+++ controllers/serverController.js
@@ -8,3 +8,17 @@ module.exports.getServerInfo = async (req, res) => {
     res.send(err);
   }
 };
+
+module.exports.createServer = async (req, res) => {
+  const { serverName } = req.body;
+  try {
+    await new serverModel({
+      serverName: serverName,
+      serverOwner: req.user.id,
+      createdAt: new Date().getDate(),
+    }).save();
+    res.status(200).json({ response: "server created." });
+  } catch (err) {
+    res.json(err);
+  }
+};
--- routes/serverRoute.js
+++ routes/serverRoute.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
 
-const { getServerInfo } = require("../controllers/serverController");
+const {
+  getServerInfo,
+  createServer,
+} = require("../controllers/serverController");
+const { verifyToken } = require("../middlewares/authJwt");
 
 router.route("/:serverId").get(getServerInfo);
+router.route("/create-server").post(verifyToken, createServer);
 
 module.exports = router;
