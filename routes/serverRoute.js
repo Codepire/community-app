16016b643459b8ae27c7c191f781e91fe60fabe7
Viral Rupani <viralrupani12017@gmail.com>
2023-07-22 19:35:33 +0530

    [fix]: redesign server endpoints

--- routes/serverRoute.js
+++ routes/serverRoute.js
@@ -8,11 +8,11 @@ const {
 } = require("../controllers/serverController");
 const { verifyToken } = require("../middlewares/authJwt");
 
+router.route("/").post(verifyToken, createServer);
 router
   .route("/:serverId")
   .get(getServerInfo)
   .delete(verifyToken, deleteServer)
   .patch(verifyToken, editServer);
-router.route("/create-server").post(verifyToken, createServer);
 
 module.exports = router;
