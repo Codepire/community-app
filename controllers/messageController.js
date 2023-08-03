29a1682ee8b3294966aca595c5e59ae427586159
Viral Rupani <viralrupani12017@gmail.com>
2023-08-03 14:25:59 +0530

    feat(messageController): add ACID properties

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,33 +1,6 @@
 const { Message } = require("../models/message.model");
 const { Channel } = require("../models/channel.model");
-
-module.exports.createMessage = async (req, res) => {
-  try {
-    const { message, channelId } = req.body;
-    const channel = await Channel.findById(channelId);
-    if (channel) {
-      const newMsg = new Message({
-        message: message,
-        createdAt: new Date().getDate(),
-        authorId: req.user.id,
-        channelId: channelId,
-      });
-      await newMsg.save();
-
-      channel.messagesId.push(newMsg._id);
-      await channel.save();
-
-      res.status(201).json({ response: "Message created" });
-    } else {
-      res.json({ response: "Channel does not exist" });
-    }
-  } catch (err) {
-    res.json({
-      response: "Some error occurred while creating message",
-      err: err,
-    });
-  }
-};
+const { default: mongoose } = require("mongoose");
 
 module.exports.getMessages = async (req, res) => {
   try {
@@ -39,5 +12,43 @@ module.exports.getMessages = async (req, res) => {
       response: "Some error occurred while retriving messages.",
       err: err,
     });
+  }
+};
+
+module.exports.createMessage = async (req, res) => {
+  const { message, channelId } = req.body;
+
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const channel = await Channel.findById(channelId).session(session);
+    if (!channel) {
+      await session.abortTransaction();
+      session.endSession();
+      return res.status(404).json({ response: "Channel does not exist" });
+    }
+
+    const newMsg = new Message({
+      message: message,
+      createdAt: new Date().getDate(),
+      authorId: req.user.id,
+      channelId: channelId,
+    });
+    await newMsg.save({ session: session });
+
+    channel.messagesId.push(newMsg._id);
+    await channel.save({ session: session });
+
+    await session.commitTransaction();
+    res.status(201).json({ response: "Message created" });
+  } catch (err) {
+    await session.abortTransaction();
+    res.json({
+      response: "Some error occurred while creating message",
+      err: err,
+    });
+  } finally {
+    session.endSession();
   }
 };
