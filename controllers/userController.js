14c2bf69acd998f7b46ab33b9dd4f587d98e75de
Viral Rupani <viralrupani12017@gmail.com>
2023-08-03 16:52:34 +0530

    feat(userController): add ACID properties

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const { User } = require("../models/user.model");
 const { serverModel } = require("../models/server.model");
+const { default: mongoose } = require("mongoose");
 
 module.exports.userInfo = async (req, res) => {
   let user = req.user;
@@ -41,51 +42,106 @@ module.exports.deleteUser = async (req, res) => {
 };
 
 module.exports.joinServer = async (req, res) => {
-  const user = await User.findById(req.user.id);
-  const server = await serverModel.findById(req.params.serverId);
+  const session = await mongoose.startSession();
+  session.startTransaction();
 
-  if (
-    user.joinedServersId.includes(server._id) &&
-    server.membersId.includes(user._id)
-  ) {
-    res.json({ response: "You have already joined this server!" });
-  } else {
-    user.joinedServersId.push(server._id);
-    await user.save();
+  const user = await User.findById(req.user.id).session(session);
+  const server = await serverModel
+    .findById(req.params.serverId)
+    .session(session);
 
-    server.membersId.push(user._id);
-    await server.save();
+  if (!server) {
+    await session.abortTransaction();
+    session.endSession();
+    return res.status(404).json({ response: "Server not found!" });
+  }
 
-    res.status(201).json({ response: "Server joined." });
+  try {
+    if (
+      user.joinedServersId.includes(server._id) &&
+      server.membersId.includes(user._id)
+    ) {
+      await session.abortTransaction();
+      res.json({ response: "You have already joined this server!" });
+    } else {
+      user.joinedServersId.push(server._id);
+      await user.save({ session: session });
+
+      server.membersId.push(user._id);
+      await server.save({ session: session });
+
+      await session.commitTransaction();
+      res.status(201).json({ response: "Server joined." });
+    }
+  } catch (err) {
+    await session.abortTransaction();
+    res.status(500).json({
+      response: "Some error occurred while joinning the server",
+      err: err,
+    });
+  } finally {
+    session.endSession();
   }
 };
 
 module.exports.leaveServer = async (req, res) => {
-  const user = await User.findById(req.user.id);
-  const server = await serverModel.findById(req.params.serverId);
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  const user = await User.findById(req.user.id).session(session);
+  const server = await serverModel
+    .findById(req.params.serverId)
+    .session(session);
+
+  if (!server) {
+    await session.abortTransaction();
+    session.endSession();
+    return res.status(404).json({ response: "Server not found!" });
+  }
+
   if (user.createdServersId.includes(server._id)) {
-    res
+    await session.abortTransaction();
+    session.endSession();
+    return res
       .status(400)
       .json({ response: "You can not leave server as you are owner." });
-  } else {
+  }
+
+  try {
     if (
       user.joinedServersId.includes(server._id) ||
       server.membersId.includes(user._id)
     ) {
-      await user.updateOne({
-        $pull: { joinedServersId: server._id },
-      });
+      await user.updateOne(
+        {
+          $pull: { joinedServersId: server._id },
+        },
+        { session: session }
+      );
 
-      await server.updateOne({
-        $pull: { membersId: user._id },
-      });
+      await server.updateOne(
+        {
+          $pull: { membersId: user._id },
+        },
+        { session: session }
+      );
 
+      await session.commitTransaction();
       res.sendStatus(204);
     } else {
+      await session.abortTransaction();
       res.status(404).json({
         response: "You haven't joined server yet. you cant leave it bruh.",
       });
     }
+  } catch (err) {
+    await session.abortTransaction();
+    res.status(500).json({
+      response: "Some error occurred while leaving the server",
+      err: err,
+    });
+  } finally {
+    session.endSession();
   }
 };
 
